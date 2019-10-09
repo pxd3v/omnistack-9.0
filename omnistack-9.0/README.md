f2779c3296c284f5f0cfa176b93763abdc8b513b
pxdrinh7 <55694628+pxdrinh7@users.noreply.github.com>
2019-10-09 18:03:23 -0300

    Add files via upload

---
+++ omnistack-9.0/README.md
@@ -0,0 +1,2 @@
+# omnistack-9.0
+Repositório do meu código da semana omnistack 9.0.
---
+++ omnistack-9.0/mobile/src/pages/List.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
+
+export default function List() {
+    const [techs, setTechs] = useState([]);
+    
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim()); // pega a String tira virgula e espaços e grava no array
+            
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            
+            <ScrollView>
+            {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    logo: {
+        height: 42,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 50
+    },
+})
